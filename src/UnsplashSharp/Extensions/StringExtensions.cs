ce2091adbfa28938f2c1a3080d9a3ebdbdc13a44
Sebastian Jensen <apps@tsjdev-apps.de>
2023-10-08 07:16:13 +0200

    Create StringExtensions.cs

---
+++ src/UnsplashSharp/Extensions/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace UnsplashSharp.Extensions
+{
+    internal static class StringExtensions
+    {
+        internal static bool IsNeitherNullNorEmpty(this string str)
+        {
+            return !string.IsNullOrEmpty(str)
+                && !string.IsNullOrWhiteSpace(str);
+        }
+    }
+}
